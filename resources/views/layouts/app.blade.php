37d27856d63bce30f054a62d63da89d2bef928a4
milosz86 <s3agate@gmail.com>
2017-09-13 15:18:03 +0200

    zainstalowano webpacka, standard w nowszej wersji. W naszej niestety nie bylo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,140 +1,140 @@
 <!DOCTYPE html>
 <html lang="pl-PL">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'sNewag') }}</title>
+  <title>{{ config('app.name', 'sNewag') }}</title>
 
-    <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+  <!-- Styles -->
+  <link href="/css/app.css" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
+  <!-- Scripts -->
+  <script>
+  window.Laravel = <?php echo json_encode([
+    'csrfToken' => csrf_token(),
+  ]); ?>
+  </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+  <div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+          <!-- Collapsed Hamburger -->
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'sNewag') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Serwisy<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('services') }}">Wszystkie serwisy</a></li>
-                        <li><a href="{{ URL::to('services/create') }}">Dodaj serwis</a></li>
-                      </ul>
-                    </li>
-
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Części<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('parts') }}">Części w Bazie Danych</a></li>
-                        <li><a href="{{ URL::to('parts/create') }}">-> Dodaj część do Bazy</a></li>
-                        <li><a href="{{ URL::to('shipments') }}">Wszystkie Przesłane/Wykorzystane</a></li>
-                        <li><a href="{{ URL::to('shipments/create') }}">-> Zmień Stan Magazynowy</a></li>
-                        <li><a href="{{ URL::to('stock') }}">Stan Magazynowy</a></li>
-                      </ul>
-                    </li>
-
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pojazdy Trakcyjne<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('vehicles') }}">Wszystkie Pojazdy</a></li>
-                        <li><a href="{{ URL::to('vehicles/create') }}">Dodaj pojazd</a></li>
-                      </ul>
-                    </li>
-
-
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Samochody<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('cars') }}">Lista pojazdów</a></li>
-                        <li><a href="{{ URL::to('cars/create') }}">Dodaj pojazd</a></li>
-                      </ul>
-                    </li>
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Logowanie</a></li>
-                            <li><a href="{{ url('/register') }}">Rejestracja</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }}&nbsp{{Auth::user()->surname}} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-
-                                  <li><a href="{{ url('/profile') }}">Profil</a></li>
-
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Wyloguj
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <!-- will be used to show any messages -->
-
-
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                @if (Session::has('message'))
-                    <div class="alert alert-danger">{{ Session::get('message') }}</div>
-                @endif
-                </div>
-            </div>
+          <!-- Branding Image -->
+          <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'sNewag') }}
+          </a>
         </div>
 
-        @yield('content')
-    </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+          <!-- Left Side Of Navbar -->
+          <ul class="nav navbar-nav">
 
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Serwisy<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="{{ URL::to('services') }}">Wszystkie serwisy</a></li>
+                <li><a href="{{ URL::to('services/create') }}">Dodaj serwis</a></li>
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Części<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="{{ URL::to('parts') }}">Części w Bazie Danych</a></li>
+                <li><a href="{{ URL::to('parts/create') }}">-> Dodaj część do Bazy</a></li>
+                <li><a href="{{ URL::to('shipments') }}">Wszystkie Przesłane/Wykorzystane</a></li>
+                <li><a href="{{ URL::to('shipments/create') }}">-> Zmień Stan Magazynowy</a></li>
+                <li><a href="{{ URL::to('stock') }}">Stan Magazynowy</a></li>
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pojazdy Trakcyjne<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="{{ URL::to('vehicles') }}">Wszystkie Pojazdy</a></li>
+                <li><a href="{{ URL::to('vehicles/create') }}">Dodaj pojazd</a></li>
+              </ul>
+            </li>
+
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Samochody<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="{{ URL::to('cars') }}">Lista pojazdów</a></li>
+                <li><a href="{{ URL::to('cars/create') }}">Dodaj pojazd</a></li>
+              </ul>
+            </li>
+
+          </ul>
+
+          <!-- Right Side Of Navbar -->
+          <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @if (Auth::guest())
+              <li><a href="{{ url('/login') }}">Logowanie</a></li>
+              <li><a href="{{ url('/register') }}">Rejestracja</a></li>
+            @else
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                  {{ Auth::user()->name }}&nbsp{{Auth::user()->surname}} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+
+                  <li><a href="{{ url('/profile') }}">Profil</a></li>
+
+                  <li>
+                    <a href="{{ url('/logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    Wyloguj
+                  </a>
+
+                  <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                  </form>
+                </li>
+              </ul>
+            </li>
+          @endif
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- will be used to show any messages -->
+
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        @if (Session::has('message'))
+          <div class="alert alert-danger">{{ Session::get('message') }}</div>
+        @endif
+      </div>
+    </div>
+  </div>
+
+  @yield('content')
+</div>
+
+<!-- Scripts -->
+<script src="/js/app.js"></script>
 </body>
 </html>
