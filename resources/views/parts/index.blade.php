8594937cb225f29391fae13ce66b2e0db4b0dcb4
milosz86 <s3agate@gmail.com>
2017-09-14 10:50:33 +0200

    wlozono tabelke z czesciami do panelu

--- resources/views/parts/index.blade.php
+++ resources/views/parts/index.blade.php
@@ -4,50 +4,59 @@
   <div class="container">
 
 
-  <h3>Dane części:</h3>
 
-<br>
+<div class="panel panel-default">
 
-  <table class="table table-responsive">
-    <thead>
-        <tr>
+  <div class="panel-body">
+    <h3>Dane części:</h3>
 
-            <td class="text-center"><h4>Nazwa części</h4></td>
-            <td class="text-center"><h4>Mianownica</h4></td>
-            <td class="text-center"><h4></h4></td>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($parts as $key => $value)
-        <tr>
+  <br>
+    <table class="table table-responsive">
+      <thead>
+          <tr>
+
+              <td class="text-center"><h4>Nazwa części</h4></td>
+              <td class="text-center"><h4>Mianownica</h4></td>
+              <td class="text-center"><h4></h4></td>
+          </tr>
+      </thead>
+      <tbody>
+      @foreach($parts as $key => $value)
+          <tr>
 
 
 
 
 
-            <td class="text-center">{{ $value->name }}</td>
-            <td class="text-center">{{ $value->number }}
-              {{ Form::open(array('url' => 'parts/' . $value->id)) }}
-                  {{ Form::hidden('_method', 'DELETE') }}
-                  {{ Form::submit('Usuń wpis', array('class' => 'btn btn-xs btn-danger')) }}
-              {{ Form::close() }}
-            </td>
+              <td class="text-center">{{ $value->name }}</td>
+              <td class="text-center">{{ $value->number }}
+                {{ Form::open(array('url' => 'parts/' . $value->id)) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit('Usuń wpis', array('class' => 'btn btn-xs btn-danger')) }}
+                {{ Form::close() }}
+              </td>
 
-            <!-- we will also add show, edit, and delete buttons -->
-            <td class="text-center">
-                <div class="btn-group-vertical">
-                <!-- show the part (uses the show method found at GET /parts/{id} -->
-                <a class="btn btn-xs btn-success" href="{{ URL::to('parts/' . $value->id) }}">Szczegóły</a>
+              <!-- we will also add show, edit, and delete buttons -->
+              <td class="text-center">
+                  <div class="btn-group-vertical">
+                  <!-- show the part (uses the show method found at GET /parts/{id} -->
+                  <a class="btn btn-xs btn-success" href="{{ URL::to('parts/' . $value->id) }}">Szczegóły</a>
 
-                <!-- edit this part (uses the edit method found at GET /parts/{id}/edit -->
-                <a class="btn btn-xs btn-info" href="{{ URL::to('parts/' . $value->id . '/edit') }}">Edytuj</a>
-              </div><br>
+                  <!-- edit this part (uses the edit method found at GET /parts/{id}/edit -->
+                  <a class="btn btn-xs btn-info" href="{{ URL::to('parts/' . $value->id . '/edit') }}">Edytuj</a>
+                </div><br>
 
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+              </td>
+          </tr>
+      @endforeach
+      </tbody>
+  </table>
+  </div>
+  <div class="panel-footer">
+
+  </div>
+</div>
+
 
   </div>
 @endsection
